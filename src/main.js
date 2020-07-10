62d2b5acdf137e097c5a8427c153cf79983e2484
toni <ferreirocouto@gmail.com>
2020-07-10 21:40:06 +0200

    correcciones header

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'bootstrap'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
 import './assets/scss/styles.scss'
 
-window.$ = window.jQuery = require('jquery');
+import 'jquery/dist/jquery.min.js'
+import 'popper.js/dist/popper.js'
+import 'bootstrap/dist/js/bootstrap.js'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 Vue.config.productionTip = false
 
