0ee8736a21590ea4699393cd32f34aefb0061ef2
toni <ferreirocouto@gmail.com>
2020-07-28 00:19:24 +0100

    cambios responsive

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,12 +2,12 @@
         <footer class="footer_area p_120" id="footer">
             <div class="container">
                 <div class="row footer_inner">
-                    <div class="col-lg-3 col-sm-12">
+                    <div class="col-lg-3 col-sm-12 footer-m">
                         <aside class="f_widget social_widget">
                             <img class="foto-footer" src="../assets/img/footer/toni.jpg" alt="">
                         </aside>
                     </div>
-                    <div class="col-lg-6 col-sm-12">
+                    <div class="col-lg-6 col-sm-12 footer-m">
                         <aside class="f_widget ab_widget">
                             <div class="f_title">
                                 <h3>{{text1}}</h3>
@@ -16,7 +16,7 @@
                             <p>{{text3}}</p>
                     </aside>
                     </div>
-                    <div class="col-lg-3 col-sm-12">
+                    <div class="col-lg-3 col-sm-12 footer-m">
                         <aside class="f_widget social_widget">
                             <div class="f_title">
                                 <h3>{{text4}}</h3>
@@ -24,6 +24,7 @@
                             <ul class="list">
                                 <li><a href="https://www.github.com/toniferr" target="_blank"><i class="fab fa-github"></i></a></li>
                                 <li><a href="https://es.linkedin.com/in/antonio-ferreiro-couto" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+                                <li><a href="mailto:ferreirocouto@gmail.com" target="_blank"><i class="far fa-envelope"></i></a></li>
                                 <li><a href="https://www.instagram.com/toni_ferreiro" target="_blank"><i class="fab fa-instagram"></i></a></li>
                                 <li><a href="https://twitter.com/toni_ferr" target="_blank"><i class="fab fa-twitter"></i></a></li>
                                 <li><a href="https://www.facebook.com/toni.deutsch.3" target="_blank"><i class="fab fa-facebook"></i></a></li>
