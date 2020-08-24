0b67367ebfdb64c62fec2d3c6f4d285400686d18
toni <ferreirocouto@gmail.com>
2020-08-24 15:18:53 +0100

    añadido router

--- src/main.js
+++ src/main.js
@@ -10,10 +10,12 @@ import "bootstrap/dist/css/bootstrap.css"
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 import i18n from './i18n'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
   i18n,
+  router,
   render: h => h(App)
 }).$mount('#app')
