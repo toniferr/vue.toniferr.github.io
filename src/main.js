7924bf3e9b0e3048b52c4d3b4a58a6c7a47b6af1
toni <ferreirocouto@gmail.com>
2020-08-24 13:46:34 +0100

    actualizacion vue y añadido i18n

--- src/main.js
+++ src/main.js
@@ -9,9 +9,11 @@ import "bootstrap/dist/css/bootstrap.css"
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  i18n,
+  render: h => h(App)
 }).$mount('#app')
