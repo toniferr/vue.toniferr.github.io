a478a5fdd63f02a5dec0d954b98a9752fb750fc8
toni <ferreirocouto@gmail.com>
2020-08-25 14:05:46 +0200

    correcciones texto iconos

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -9,9 +9,9 @@
 								<h2>{{ $t('banner.name') }}</h2>
 								<h5>{{ $t('banner.job') }}</h5>
 								<div class="row">
-								<h2><a class="social-web-banner-m" href="https://www.github.com/toniferr" target="_blank"><i class="fab fa-github social-web-banner"></i></a></h2>
-                                <h2><a class="social-web-banner-m" href="https://es.linkedin.com/in/antonio-ferreiro-couto" target="_blank"><i class="fab fa-linkedin social-web-banner"></i></a></h2>
-                                <h2><a class="email social-web-banner-m" href="mailto:ferreirocouto@gmail.com" target="_blank"><i class="fas fa-envelope social-web-banner"></i></a></h2>
+								<h3><a class="social-web-banner-m" href="https://www.github.com/toniferr" target="_blank"><i class="fab fa-github social-web-banner"></i></a></h3>
+                                <h3><a class="social-web-banner-m" href="https://es.linkedin.com/in/antonio-ferreiro-couto" target="_blank"><i class="fab fa-linkedin social-web-banner"></i></a></h3>
+                                <h3><a class="email social-web-banner-m" href="mailto:ferreirocouto@gmail.com" target="_blank"><i class="fas fa-envelope social-web-banner"></i></a></h3>
 								</div>
 							</div>
 						</div>
