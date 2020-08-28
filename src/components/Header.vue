2b7c9c5b5f97d13d807d1f84e2cf1cbf94bb781e
toni <ferreirocouto@gmail.com>
2020-08-28 12:34:12 +0100

    cambio logo y tamaños de divs

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,8 @@
                 <nav class="navbar navbar-expand-lg navbar-light">
                   <div class="container box_1620">
                     <!-- Brand and toggle get grouped for better mobile display -->
-                    <a class="navbar-brand logo_h"><b class="titulo-nombre">FERREIRO</b></a>  
+                    <a class="navbar-brand logo_h"><img src="../assets/img/logo.png" alt=""></a>
+                    <!--<a class="navbar-brand logo_h"><b class="titulo-nombre">FERREIRO</b></a>  -->
                                     
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="icon-bar"></span>
