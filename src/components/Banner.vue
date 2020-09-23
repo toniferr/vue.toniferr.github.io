0e54a505c68e013aa8d91deb67b6dae4b3e58aaf
toni <ferreirocouto@gmail.com>
2020-09-23 15:24:53 +0100

    cambios en banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -3,16 +3,19 @@
             <div class="banner_inner">
 				<div class="container">
 					<div class="row">
-						<div class="col-lg-9">
+						<div class="col-lg-3">
+						</div>
+						<div class="col-lg-6">
 							<div class="banner_content home_left_img">
 								<h5>{{ $t('banner.hi') }}</h5>
 								<h2>{{ $t('banner.name') }}</h2>
 								<h5>{{ $t('banner.job') }}</h5>
-								<div class="row">
-								<h4><a class="social-web-banner-m" href="https://www.github.com/toniferr" target="_blank"><i class="fab fa-github social-web-banner"></i></a></h4>
-                                <h4><a class="social-web-banner-m" href="https://es.linkedin.com/in/antonio-ferreiro-couto" target="_blank"><i class="fab fa-linkedin social-web-banner"></i></a></h4>
-                                <h4><a class="email social-web-banner-m" href="mailto:ferreirocouto@gmail.com" target="_blank"><i class="fas fa-envelope social-web-banner"></i></a></h4>
-								</div>
+								<h4>
+									<a class="social-web-banner-m" href="https://www.github.com/toniferr" target="_blank"><i class="fab fa-github social-web-banner"></i></a>
+									<a class="social-web-banner-m" href="https://es.linkedin.com/in/antonio-ferreiro-couto" target="_blank"><i class="fab fa-linkedin social-web-banner"></i></a>
+									<a class="email social-web-banner-m" href="mailto:ferreirocouto@gmail.com" target="_blank"><i class="fas fa-envelope social-web-banner"></i></a>
+								</h4>
+								
 							</div>
 						</div>
 						<div class="col-lg-3">
