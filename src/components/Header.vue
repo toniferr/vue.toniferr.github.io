5d93d90821b54ab132511d8c1187d8d734a490db
toni <ferreirocouto@gmail.com>
2020-10-06 20:26:56 +0100

    cambio logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
                 <nav class="navbar navbar-expand-lg navbar-light">
                   <div class="container">
                     <!-- Brand and toggle get grouped for better mobile display -->
-                    <a class="navbar-brand logo_h"><img src="../assets/img/logo.png" alt=""></a>
+                    <a class="navbar-brand logo_h"><img src="../assets/logo.png" alt=""></a>
                     <!--<a class="navbar-brand logo_h"><b class="titulo-nombre">FERREIRO</b></a>  -->
                                     
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
